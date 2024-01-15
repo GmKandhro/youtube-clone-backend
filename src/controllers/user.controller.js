d07de2badd0738d7fc02c940ae85fe6f1f7a48c4
Gm kandhro <gmkandhro182@gmail.com>
2024-01-15 16:07:24 +0500

    complete controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const generateAccessAndRefereshTokens = async(userId) =>{
     }
 }
 
-const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler( async (req,res)=>{
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
@@ -35,58 +35,55 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return res
 
+    const {username, email,fullName,password} = req.body
 
-    const {fullName, email, username, password } = req.body
-    //console.log("email: ", email);
-
-    if (
-        [fullName, email, username, password].some((field) => field?.trim() === "")
-    ) {
-        throw new ApiError(400, "All fields are required")
+    if (username == '' || fullName == '' || email == "" || password == "") {
+        throw new ApiError('400','all fileds are required')
     }
 
-    const existedUser = await User.findOne({
-        $or: [{ username }, { email }]
+   const existedUserWithEmail = await User.findOne({
+    email
+    })
+   const existedUserWithUsername = await User.findOne({
+    username
     })
 
-    if (existedUser) {
-        throw new ApiError(409, "User with email or username already exists")
+    if(existedUserWithEmail){
+        throw new ApiError(409,`user with ${email} email already exists`)
     }
-    //console.log(req.files);
+    if(existedUserWithUsername){
+        throw new ApiError(409,`user with ${username} username already exists`)
+    }
+   
+     if(!Array.isArray(req.files.avatar)){
+        throw new ApiError('400', "Avatar file is required")
+    }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
+    
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
-    
 
-    if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required")
-    }
-
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+   
+    let avatar = await uploadOnCloudinary(req.files.avatar[0].path)
+    let coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
-        throw new ApiError(400, "Avatar file is required")
+        throw new ApiError('400', "Avatar file is required")
     }
-   
 
     const user = await User.create({
         fullName,
-        avatar: avatar.url,
+        avatar:avatar?.url,
         coverImage: coverImage?.url || "",
-        email, 
+        email,  
         password,
         username: username.toLowerCase()
+
     })
 
-    const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
-    )
+    const createdUser = await User.findById(user._id).select(  "-password -refreshToken")
 
     if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user")
@@ -95,8 +92,9 @@ const registerUser = asyncHandler( async (req, res) => {
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
+    
 
-} )
+})
 
 const loginUser = asyncHandler(async (req, res) =>{
     // req body -> data
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,47 +1,113 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
+  let { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  const allVideos = await Video.aggregate([
+    {
+      $match: {
+        userId: new mongoose.Types.ObjectId(userId),
+      }
+    }
+  ]).skip(skip).limit(limit)
+
+  
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
-})
+  // TODO: get video, upload to cloudinary, create video
+  const { title, description, thumbnail, videoFile } = req.body;
+  // validate data
+  // upload thumbnail and video file to cloudinary
+  // create video
+  // return video
+  // console.log(title, description, thumbnail, videoFile)
+
+  if (title === "" || description === "") {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  let thumbnailUrl = await uploadOnCloudinary(req.files.thumbnail[0].path);
+  let videoFileUrl = await uploadOnCloudinary(req.files.videoFile[0].path);
+
+  // console.log(req.files)
+  if (!videoFileUrl || !thumbnailUrl) {
+    throw new ApiError(400, "Video file and thumbnail are required");
+  }
+  // console.log(videoFileUrl.url , videoFileUrl.duration)
+
+  const video = await Video.create({
+    title,
+    description,
+    thumbnail: thumbnailUrl.url,
+    videoFile: videoFileUrl.url,
+    duration: videoFileUrl.duration,
+    owner:req.user._id
+  });
+  res.status(201).json(new ApiResponse(201, "Video created", video));
+});
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+  //TODO: get video by id
+  const { videoId } = req.params;
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+  res.status(200).json(new ApiResponse(200, "Video found", video));
+});
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
+  //TODO: update video details like title, description, thumbnail
+  const { videoId } = req.params;
+  const { title, description, thumbnail } = req.body;
+  const video = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: { title, description, thumbnail },
+    },
+    { new: true }
+  );
 
-})
+  res.status(200).json(new ApiResponse(200, "Video updated", video));
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
-})
+  //TODO: delete video
+  const { videoId } = req.params;
+  const video = await Video.findByIdAndDelete(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+  res.status(200).json(new ApiResponse(200, "Video deleted"));
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+  const { videoId } = req.params;
+  const video = await Video.findOneAndUpdate({ _id: videoId }, [
+    { $set: { isPublished: { $eq: [false, "$isPublished"] } } },
+  ]);
+  if (!video) {
+      throw new ApiError(404, "Video not found");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Video isPublished status updated", video));
+});
 
 export {
-    getAllVideos,
-    publishAVideo,
-    getVideoById,
-    updateVideo,
-    deleteVideo,
-    togglePublishStatus
-}
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
